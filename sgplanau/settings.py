fe2d7c0d61d1837aa00c3b36bf7d5ad4395a4c7d
pokkew <brunogabrielpk@gmail.com>
2019-10-22 08:32:32 -0400

    crispy forms and some bootstrap

--- sgplanau/settings.py
+++ sgplanau/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'planaula',
     'bootstrap4',
+    'crispy_forms',
 
 
 ]
@@ -122,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
